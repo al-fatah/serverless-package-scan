f50d863fd2b9bf30b0a991a930f9773bfefd5092
al-fatah <alfatah.din@gmail.com>
2025-11-05 20:58:44 +0800

    update

--- provider.tf
+++ provider.tf
@@ -2,10 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.0"
     }
   }
 }
 
 # Configure the AWS Provider
-provider "aws" {}
+provider "aws" {
+  region = "us-east-1"
+}
